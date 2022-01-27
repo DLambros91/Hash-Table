6a04d78bec164a908e450eee9095637a64a707a4
Demetrios <d.lambropoulos91@gmail.com>
2022-01-26 22:06:37 -0500

    Update hashtable.hpp

--- hashtable.hpp
+++ hashtable.hpp
@@ -1,6 +1,14 @@
-template <typename T>
-class hashtable {
-    public: 
 
-    private: 
-};
+namespace myhash {
+    template <typename T>
+    class hashtable {
+        public: 
+            T get (int key);
+            bool exists (int key);
+            void remove (int key);
+            void add (int key, T value);
+            int hash (int k, int m);
+        private: 
+    };
+
+}
