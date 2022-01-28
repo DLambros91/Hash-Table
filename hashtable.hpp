14fb86b7c594cb4052885b16801b40a497fe86fb
Demetrios <d.lambropoulos91@gmail.com>
2022-01-27 23:52:33 -0500

    Removal of .tpp
    Defining all functions in the .hpp header file

--- hashtable.hpp
+++ hashtable.hpp
@@ -1,14 +1,25 @@
+#ifndef MY_HASH
+#define MY_HASH
+#include <iostream>
 
-namespace myhash {
-    template <typename T>
-    class hashtable {
-        public: 
-            T get (int key);
-            bool exists (int key);
-            void remove (int key);
-            void add (int key, T value);
-            int hash (int k, int m);
-        private: 
-    };
+template <typename T>
+class hashtable {
+    public: 
+        hashtable () {
+            m = 100;
+            arr = new T[m];
+        }
 
-}
+        T get (int key);
+        bool exists (int key);
+        void remove (int key);
+        void add (T key, T value);
+        int hash (T k, int m) {
+            return std::hash<T>(k) % m;
+        }
+    private: 
+        T * arr;
+        int m;
+};
+
+#endif
